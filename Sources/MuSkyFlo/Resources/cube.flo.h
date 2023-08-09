b30c2e2598fc9039fa53999bb8d92e8e46021929
musesum <musesum@gmail.com>
2023-08-09 17:09:17 +0800

    refactor matrices

--- Sources/MuSkyFlo/Resources/cube.flo.h
+++ Sources/MuSkyFlo/Resources/cube.flo.h
@@ -1,7 +1,7 @@
 model.canvas {
     cube {
         motion (%2~1)
-        rotate (x -1…1~0, y -1…1~0)
+        rotate (x -1…1~0, y -1…1~0) <> shader.render.cubemap.rotate
         back (%2~1)
         show (%2~0) <> shader.render.cubemap.on
     }
@@ -9,7 +9,7 @@ model.canvas {
 _menu.canvas {
     cube (sym "cube") {
         motion (sym "gyroscope")
-        rotate (svg "icon.direction")
+        rotate (sym "rotate.3d")
         back   (sym "cube")
         show   (sym "cube.fill")
     }
