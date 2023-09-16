85ece220437c87fae86341da842be35bb19ad0f5
musesum <musesum@gmail.com>
2023-09-15 19:36:12 -0700

    refactor -storyboard vc

--- Sources/MuSkyFlo/Resources/shader.flo.h
+++ Sources/MuSkyFlo/Resources/shader.flo.h
@@ -7,14 +7,13 @@ shader {
         plato
     }
     cell {
-        fade  (1.2…3  ) >> on(1) { on(%2) }
-        ave   (0…1=0.5) >> on(1) { on(%2) }
-        melt  (0…1=0.5) >> on(1) { on(%2) }
-        tunl  (0…5=1  ) >> on(1) { on(%2) }
-        slide (0…7=3  ) >> on(1) { on(%2) }
-        fred  (0…4=4  ) >> on(1) { on(%2) }
-        zha   (0…6=2  ) >> on(1) { on(%2) bits(2…4=3) loops(11) }
-        cell˚on >> cell˚on(0)
+        fade  (1.2…3  ) >> on(1) { on(%2) >> cell˚on(0) }
+        ave   (0…1=0.5) >> on(1) { on(%2) >> cell˚on(0) }
+        melt  (0…1=0.5) >> on(1) { on(%2) >> cell˚on(0) }
+        tunl  (0…5=1  ) >> on(1) { on(%2) >> cell˚on(0) }
+        slide (0…7=3  ) >> on(1) { on(%2) >> cell˚on(0) }
+        fred  (0…4=4  ) >> on(1) { on(%2) >> cell˚on(0) }
+        zha   (0…6=2  ) >> on(1) { on(%2) >> cell˚on(0) bits(2…4=3) loops(11) }
     }
     compute {
         draw (x 0…1~0.5, y 0…1~0.5) { on(0…1~1) }
@@ -34,8 +33,8 @@ shader {
             frame(x 0, y 0, w 1080, h 1920)
             repeat(x -1…1~0, y -1…1~0)
             mirror(x  0…1~0, y  0…1~0)
+            rotate(x  0…1~0, y  0…1~0)
             gravity(0…2~0)
-            rotate(x -1…1~0, y -1…1~0)
             on(%2~0)
         }
         plato { on(%2~0) }
